4f06d71c7922ef090a059bad5d5fd783dc3cca2b
inku0 <ingvar@saarend.com>
2025-11-10 10:54:47 +0200

    lisasin kolmanda lehe (Physicum->Delta) ning lisasin pealkirjale veel atribuudi 'algus', mis määrab ära, kumba pidi päring saadetakse

--- script.js
+++ script.js
@@ -135,12 +135,17 @@ const query = `
   }
 `;
 
+const delta =
+  "Tartu Ülikooli Delta õppehoone,  Tartu linn,  Tartu linn, Tartu maakond::58.38507,26.725327";
+const physicum =
+  "Tartu Ülikooli Physicum,  Tartu linn,  Tartu linn, Tartu maakond::58.36618,26.690229";
+
+const algus = document.getElementById("pealkiri").getAttribute("algus");
+
 // peab olema just selle nimega, muidu graphql ei aktsepteeri
 const variables = {
-  fromPlace:
-    "Tartu Ülikooli Delta õppehoone,  Tartu linn,  Tartu linn, Tartu maakond::58.38507,26.725327",
-  toPlace:
-    "Tartu Ülikooli Physicum,  Tartu linn,  Tartu linn, Tartu maakond::58.36618,26.690229",
+  fromPlace: algus == "delta" ? delta : physicum,
+  toPlace: algus == "delta" ? physicum : delta,
   numItineraries: 5, // see määrab ära, mitu bussiaega tuleb
   modes: [{ mode: "BUS" }, { mode: "WALK" }],
   date: new Date().toISOString().split("T")[0], // "2025-10-22",
